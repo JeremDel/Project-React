f60a97b07073fbee3f2716f26d9858f6be645f09
Milena <milenalonfat@gmail.com>
2023-04-24 23:44:39 +0200

    rdar premier essaie

--- src/data/userQuestionnaire.js
+++ src/data/userQuestionnaire.js
@@ -1,23 +1,31 @@
-import { db } from './initFirebase';
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
 import { getAuth } from "firebase/auth";
 
 import { produce } from "immer"
-
+import firebaseApp from '../initFirebase';
 export async function addUserQuestionnaire(questionnaire) {
+  // Get user id
+  const uid = getAuth().currentUser.uid;
 
-
-  const auth = getAuth();
-  const user = auth.currentUser;
-  const uid = user.uid;
-  const q = produce(questionnaire, draft => {
-    draft.uid = uid;
+  // Append the current date to the questionnaire
+  const newQuestionnaire = produce(questionnaire, draft => {
     draft.datetime = new Date();
-  });  
-  // questionnaire.uid = uid;
-  // questionnaire.datetime = new Date();
-  
-  const coll = collection(db, "userQuestionnaires");
-  const docRef = await addDoc(coll, q);
-  return docRef;
+  });
+
+  // Get the document with the user id from the collection userQuestionnaires
+  const myDoc = firebaseApp.firestore().collection('userQuestionnaires').doc(uid);
+  myDoc.get().then((doc) => {
+    if(doc.exists){
+      // Update the questionnaires and store them in the document
+      const data = doc.data();
+      const questionnaires = data.questionnaires;
+      const newQuestionnaires = [newQuestionnaire, ...questionnaires];
+      myDoc.update({
+        questionnaires: newQuestionnaires      });
+    } else {
+      // Create the document with the questionnaire
+      myDoc.set({
+        questionnaires: [newQuestionnaire]
+      });
+    }
+  });
 }
