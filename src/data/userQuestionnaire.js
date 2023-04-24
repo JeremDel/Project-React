97625ee8b4ab1f048058236afc177f86c5586a86
Benjamin Keller <bkeller79@hotmail.com>
2023-04-24 17:05:23 +0200

    FlowQuestionChange & bugfixes

--- src/data/userQuestionnaire.js
+++ src/data/userQuestionnaire.js
@@ -2,15 +2,22 @@ import { db } from './initFirebase';
 import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
 import { getAuth } from "firebase/auth";
 
+import { produce } from "immer"
 
 export async function addUserQuestionnaire(questionnaire) {
+
+
   const auth = getAuth();
   const user = auth.currentUser;
   const uid = user.uid;
-  questionnaire.uid = uid;
-  questionnaire.datetime = new Date();
+  const q = produce(questionnaire, draft => {
+    draft.uid = uid;
+    draft.datetime = new Date();
+  });  
+  // questionnaire.uid = uid;
+  // questionnaire.datetime = new Date();
   
   const coll = collection(db, "userQuestionnaires");
-  const docRef = await addDoc(coll, questionnaire);
+  const docRef = await addDoc(coll, q);
   return docRef;
 }
--- src/screens/QuestionnaireTheme.js
+++ src/screens/QuestionnaireTheme.js
@@ -11,12 +11,13 @@ export default class QuestionnaireTheme extends React.Component {
         this.state = {
             theme: this.props.theme,
         //   questions: this.props.theme.questions.map((e, i) => null),
-          currentQuestion: 0,
+            currentQuestion: 0,
         //   totPoints: 0,
         //   recommendations: [],
         //   valid: false
         };
         this.onQuestionStateChange = this.onQuestionStateChange.bind(this);
+        this.onFlowQuestionChange = this.onFlowQuestionChange.bind(this);
       }    
 
     themeInfo() {
@@ -73,13 +74,88 @@ export default class QuestionnaireTheme extends React.Component {
         })
     }
 
-    onFlowQuestionChange() {
+    onFlowQuestionChange(questionId, qState) {
+        console.log(questionId, qState);
+        // return;
+        this.setState(state => {
 
+            const nextState = produce(state, draftState => {
+
+                draftState.theme.questions[questionId].selectedOptions = qState;
+                draftState.theme.totPoints = draftState.theme.defaultPoints;
+                if (!state.theme.hasOwnProperty('recommendations')) {
+                    draftState.theme.recommendations = [];
+                }
+
+                console.log(state.theme.questions[questionId])
+                // return;
+                let answer = draftState.theme.questions[questionId].answers[qState[0]];
+
+                if (answer.hasOwnProperty('points')) {
+                    draftState.theme.totPoints = answer.points;
+                }
+
+                if (answer.recommendation) {
+                    draftState.theme.recommendations.push(answer.recommendation);
+                }
+                else if (answer.recommendations) {
+                    draftState.theme.recommendations = draftState.theme.recommendations.concat(answer.recommendations)
+                }
+
+                if (answer.goto) {
+                    draftState.currentQuestion = draftState.theme.questions.findIndex(element => element.id === answer.goto);
+                }
+                else {
+                    draftState.theme.valid = true;
+                }
+                // let answer
+
+                /* 
+
+                let validQuestions = 0;
+                draftState.totPoints = 0;
+                draftState.recommendations = [];
+
+                draftState.questions.forEach((q, qi) => {
+                    if (q.hasOwnProperty('selectedOptions')) {
+                        if (q.selectedOptions.length) {
+                            validQuestions++;
+                        }
+
+                        q.selectedOptions.forEach(ai => {
+                            let answer = q.answers[ai];
+                            draftState.totPoints += answer.points;
+
+                            if (answer.recommendation) {
+                                draftState.recommendations.push(answer.recommendation);
+                            }
+                            else if (answer.recommendations) {
+                                draftState.recommendations = draftState.recommendations.concat(answer.recommendations)
+                            }
+                        })
+                    }
+                })
+
+                draftState.valid = validQuestions === this.props.theme.questions.length; */
+            })
+
+            return nextState; 
+        }, () => { 
+            // this.props.onThemeChange(this.props.themeId, this.state.theme) 
+        })        
     }
 
     questions() {
         if (this.props.theme.type === 'Flow') {
-            return <></>
+            let question = this.props.theme.questions[this.state.currentQuestion];
+            return (
+                <QuestionnaireQuestion 
+                    question={ question } 
+                    questionId={ this.state.currentQuestion } 
+                    type="Single" 
+                    key={'q-' + this.state.currentQuestion} 
+                    onStateChange={ this.onFlowQuestionChange } />
+            )
         }
         else {
             let questionList = this.props.theme.questions;
